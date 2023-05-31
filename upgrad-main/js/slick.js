00283226a15474303e1c1c6742479254da2e3136
syedzaib <syedzohaib201@gmail.com>
2023-05-31 10:21:08 +0500

    slider changes

--- upgrad-main/js/slick.js
+++ upgrad-main/js/slick.js
@@ -6,25 +6,22 @@ $(".rightArrow").on("click", function () {
   $(".center").slick("slickNext");
 });
 
-var $status = $(".slide-count");
-var $slickElement = $(".center");
+var $status = $('.slide-count');
+var $slickElement = $('.center');
 
-$slickElement.on(
-  "init reInit afterChange",
-  function (event, slick, currentSlide, nextSlide) {
-    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status.text(i + " / " + slick.slideCount);
-  }
-);
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + ' / ' + slick.slideCount);
+});
 
-$(".center").slick({
+$('.center').slick({
   centerMode: true,
-  // centerPadding: "100px",
+  centerPadding: '0px',
   slidesToShow: 1,
   arrows: false,
   infinite: false,
-
+  
   //autoplay: true,
   //autoplaySpeed: '3000',
   //appendArrows: '.my-arrows',
